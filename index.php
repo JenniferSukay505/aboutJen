f961423f771a4a4aaf3c585fd0bf3dd142c0e287
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-13 23:13:58 -0600

    added gitignore and index

--- index.php
+++ index.php
@@ -1,9 +1,43 @@
-<!QDOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-   <head>
-	   <title>profile webpage</title>
-   </head>
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+		<link type="text/css" href="css/styles.css" rel="stylesheet"/>
+		<link type="text/css" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css"/>
+		<title>Jennifer Sukay</title>
+	</head>
 	<body>
-		<p>profile page</p>
+	<header class="bgimage">
+		<div class="container">
+			<a href="services.php" class="btn btn-info" role="button">Services</a>
+			<a href="registration.php" class="btn btn-info" role="button">Registration</a>
+			</div>
+	</header>
+			<main class="container">
+				<div>
+					<h1 class="animated swing">Jennifer Sukay</h1>
+						<p class="bio"><b>Biography</b></p>
+						<p class="bio"> With a degree in business administration and experience in advertising
+						and promotions, I am continuing  my education into computer technology.  I enjoy
+						bringing ideas to life. Nonprofit businesses have been my favorites to work on. I
+						collaboration and challenges.  I welcome new ideas and projects</p>
+						<img src="image/thegirl.jpg" alt="girl image"/>
+
+			<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+			<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script>
+			<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/additional-methods.min.js"></script>
+			<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+			<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+			<script src= "js/script.js"></script>
+
+			</div>
+		</main>
 	</body>
 </html>
